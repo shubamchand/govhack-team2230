9da77e45e0172f60b5a48fe31cce7df429e61df5
Shubham Chand <6082955+shubamchand@users.noreply.github.com>
2022-08-21 14:32:47 +0930

    Add files via upload

---
+++ dist/js/pre_loader.js
@@ -0,0 +1,9 @@
+function start_loader(){
+	$('body').prepend('<div id="preloader"></div>')
+}
+function end_loader(){
+	 $('#preloader').fadeOut('fast', function() {
+        $(this).remove();
+        window.location('https://www.google.com.au')
+      })
+}
